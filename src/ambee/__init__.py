fcdf350eda6da732e9a5d6257f51946d26d2cae8
Franck Nijhof <git@frenck.dev>
2021-06-09 01:44:25 +0200

    Make AmbeeAuthenticationError available from package root

--- src/ambee/__init__.py
+++ src/ambee/__init__.py
@@ -1,11 +1,12 @@
 """Asynchronous Python client for the Ambee API."""
 
-from .ambee import Ambee, AmbeeConnectionError, AmbeeError
+from .ambee import Ambee, AmbeeAuthenticationError, AmbeeConnectionError, AmbeeError
 from .models import AirQuality  # noqa
 
 __all__ = [
     "AirQuality",
     "Ambee",
+    "AmbeeAuthenticationError",
     "AmbeeConnectionError",
     "AmbeeError",
 ]
